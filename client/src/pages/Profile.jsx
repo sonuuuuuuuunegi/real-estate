ba0818e3dd2861ff9c1bed81dc2e424bee88cd12
sonuuuuuuuunegi <sachinnegi220@gmail.com>
2024-04-18 15:25:54 +0530

    add delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { useRef } from 'react';
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage';
 import {app} from '../firebase';
-import { updateUserStart,updateUserSuccess,updateUserFailure } from '../redux/user/userSlice';
+import { updateUserStart,updateUserSuccess,updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 
 
@@ -83,6 +83,24 @@ export default function Profile() {
   };
 
 
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess (data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  };
+
+
   return (
     <div className='p-3 max-w-lg mx-auto '> 
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -105,7 +123,7 @@ export default function Profile() {
        <button disabled={loading} className='bg-slate-700  text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80 '>{loading ? 'Loading...' :'update'}</button>
       </form>
       <div className='flex justify-between mt-5 '>
-        <span className='text-red-700 cursor-pointer '>Delete Account</span>
+        <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer '>Delete Account</span>
         <span className='text-red-700 cursor-pointer'>Sign Out</span>
       </div>
 
