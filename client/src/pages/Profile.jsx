fc31e68b9591fb880d366f13087e7f13c1c4f233
sonuuuuuuuunegi <sachinnegi220@gmail.com>
2024-04-18 09:19:21 +0530

    complete profile UI page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,13 +1,16 @@
 import {useSelector} from 'react-redux';
 import React from 'react'
+import { useRef } from 'react';
 
 export default function Profile() {
   const {currentUser}=useSelector((state)=>state.user);
+  const fileRef=useRef(null);
   return (
     <div className='p-3 max-w-lg mx-auto '> 
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form className='flex flex-col gap-4 '>
-        <img className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2  ' src={currentUser.avatar} alt="Profile"  />
+        <input type="file" ref={fileRef} hidden/>
+        <img onClick={()=>fileRef.current.click()} className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2  ' src={currentUser.avatar} alt="Profile"  />
         <input  type="text" placeholder='username' id='username' className='border p-3 rounded-lg ' />
         <input  type="email" placeholder='email' id='email' className='border p-3 rounded-lg ' />
         <input  type="password" placeholder='password' id='password' className='border p-3 rounded-lg ' />
