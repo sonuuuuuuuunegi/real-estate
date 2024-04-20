a64d122b7b77ee4f4b7b47d4b138093edd1fbd44
sonuuuuuuuunegi <sachinnegi220@gmail.com>
2024-04-20 18:31:04 +0530

    complete listing home and about

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,10 @@ import About from './pages/About';
 import Home from './pages/Home';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
+import CreateListing from './pages/createListing';
+import UpdateListing from './pages/UpdateListing';
+import Listing from './pages/Listing';
+import Search from './pages/Search';
 
 export default function App() {
   return (
@@ -16,8 +20,14 @@ export default function App() {
     <Route path='/sign-in' element={<SignIn/>}></Route>
     <Route path='/sign-up' element={<SignUp/>}></Route>
     <Route path='/about' element={<About/>}></Route>
+    <Route path='search' element={<Search/>}></Route>
+    <Route path='/listing/:id' element={<Listing/>}></Route>
+
     <Route element={<PrivateRoute/>}>
     <Route path='/profile' element={<Profile/>}/>
+    <Route path='/create-listing' element={<CreateListing/>}/>
+    <Route path='/update-listing/:id' element={<UpdateListing/>}/>
+    
     </Route>
     
     
